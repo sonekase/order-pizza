61f96622f2eeea05f92ab1f6d5b412d702e6bef8
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-06-15 10:21:22 -0700

    Enter code for scroll feature in scripts.js, it works.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,13 @@
 
 // user interface
 $(document).ready(function(){
-
+  $("button#scroll").click(function(){
+  $('html, body').animate({scrollTop:$(document).height()
+  }, 'slow');
+  });
+  $("#order-area").submit(funtion(event){
+    event.preventDefault();
+  });
 
 
 });
